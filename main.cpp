a335b4113a79d9cec102d22fee1526fe280bf48b
Kaz Piros <117852608+Kazpiros@users.noreply.github.com>
2025-03-17 23:49:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 /*
- * ADC.cpp
+ * main.cpp
  *
  * Created: 3/11/2025 6:23:00 PM
  * Author : Kazfp
  *
- *
- * helpful links "how to use adc in avr micro controllers"
  */ 
 
 #include <avr/io.h>
@@ -14,48 +12,28 @@
 
 #include <util/delay.h>
 #include "USART.h"
-
-
-
-// ADC module 24.9
-void ADC_init(void)
-{
-	// --- 24.9.1 ADC MUX selection, 
-	//Vcc external ref | ADLAR-def. order | Mux0 (ADC0)
-	ADMUX = (1 << MUX0); 
-	
-	//---  ADCSRA (also where interrupts hide)
-	//ADC-EN | pre-scaler
-	ADCSRA = (1 << ADEN) | (1 << ADPS2);
-}
-
-// reads into a 16 bit val
-uint16_t ADC_Read(void) 
-{
-	ADCSRA |= (1 << 6); //start
-	while( (ADCSRA & (1 << 6)) == 1 );
-	//uint8_t adcl = ADCL; //read 
-	//uint8_t adch = ADCH; //read  
-	// these probably take a lot of time to do, check if compiler rids them
-	uint16_t val = ((ADCH << 8) | ADCL) & 0x3FF; 
-	return val;
-}
+#include "ADC.h"
+#include "PWM.h"
 
 unsigned int ADC_VAL = 0;
 char ADC_VAL_Char[5];
 
 int main(void)
 {
-	// look into debug mode, using a button to step thru code.
-	PRR = 0xef; // all power reduction measures off
-	USART_init(9600);
-	ADC_init();
+	//PRR = 0xff; // all power reduction measures off
+	USART_init(207);
+	PWM_init();
+	ADC_0_init();
+	
+	ADC_0_start_conversion(0);
+	register int a = 0;
     while (1) 
     {
-		 // " / 4" to reduce accuracy to fit into UART
-		ADC_VAL = ADC_Read()  >> 2;
-		USART_Transmit(ADC_VAL);
-		_delay_ms(200);
+		start_timer();
+		a = ADC_0_get_conversion(0);
+		PWM_update(a);
+		a = stop_timer();
+		USART_Transmit(a); // debug
     }
 }
 
