84239f987deaddf94cfa7a2c71b0f1655edf7688
Kaz Piros <117852608+Kazpiros@users.noreply.github.com>
2025-03-17 23:51:56 -0700

    Create circular_buffer.h

---
+++ circular_buffer.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 16U
+#define BUFFER_SIZE_MASK (BUFFER_SIZE-1U)
+
+typedef struct circular_buf{
+    uint16_t buffer[BUFFER_SIZE];
+    int readIndex;
+    int writeInd;
+}circular_buf;
+
+circular_buf buffer = { .writeIndex = 0 }; // malloc bad
+
+inline void write_cbuf(circular_buf* cbuf, int val)
+{
+    //masking done as an alternative to "if at end, go to start"
+    cbuf->buffer[(++cbuf->writeIndex) & BUFFER_SIZE_MASK] = val;
+}
+
+// Xn is reads value at index most recently written, counting down
+inline int read_cbuf(circular_buf* cbuf, unsigned Xn)
+{
+    return cbuf->buffer[(cbuf->writeIndex - Xn) & BUFFER_SIZE_MASK];
+}
+
+//can and will be just pasted into final product, removal of function.
+void cbuf_init(circular_buf* cbuf)
+{
+    cbuf->writeIndex = 0;
+    cbuf->readIndex = 0;
+}
